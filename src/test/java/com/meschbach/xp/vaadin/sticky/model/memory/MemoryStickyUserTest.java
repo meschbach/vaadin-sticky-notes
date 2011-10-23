484450ac4bbbb519dc44ca6b210a5d3b266c2cab
Mark Eschbach <meschbach@gmail.com>
2011-10-23 09:26:55 -0700

    Modified POM to ensure that TestNG works as expected

---
+++ src/test/java/com/meschbach/xp/vaadin/sticky/model/memory/MemoryStickyUserTest.java
@@ -0,0 +1,18 @@
+/*
+ */
+package com.meschbach.xp.vaadin.sticky.model.memory;
+
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author "Mark Eschbach" (meschbach@gmail.com)
+ */
+public class MemoryStickyUserTest {
+
+    @Test
+    public void iamATest(){
+	
+    }
+}
